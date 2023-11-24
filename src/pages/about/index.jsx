9e301296245f2c0a52c03db612d9d3754ae4ab3c
Jonah <jonah.wakahiumaina@gmail.com>
2023-11-24 10:40:01 +0300

    created home and about page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,4 +1,76 @@
+import { FaDownload } from "react-icons/fa";
+import Info from "../../components/Info";
+import Stats from "../../components/Stats";
+import Skills from "../../components/Skills";
+import { resume } from "../../data";
+import ResumeItem from "../../components/ResumeItem";
+import "./about.css";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <main className="section container">
+      <section className="about">
+        <h2 className="section_title">
+          About <span>Me</span>
+        </h2>
+
+        <div className="about_container grid">
+          <div className="about_info">
+            <h3 className="section_subtitle">Personal Infos</h3>
+
+            <ul className="info_list grid">
+              <Info />
+            </ul>
+            <a href="" className="button">
+              Download Cv
+              <span className="button_icon">
+                <FaDownload />
+              </span>
+            </a>
+          </div>
+
+          <div className="stats grid">
+            <Stats />
+          </div>
+        </div>
+
+        <div className="separator"></div>
+
+        <section className="skills">
+          <h3 className="section_subtitle subtitle-center">My Skills</h3>
+
+          <div className="skills_container grid">
+            <Skills />
+          </div>
+        </section>
+
+        <div className="separator"></div>
+
+        <section className="resume">
+          <h3 className="section_subtitle subtitle-center">
+            Experience & Education
+          </h3>
+
+          <div className="resume_container grid">
+            <div className="resume_data">
+              {resume.map((val) => {
+                if (val.category === "experience") {
+                  return <ResumeItem key={val.id} {...val} />;
+                }
+              })}
+            </div>
+
+            <div className="resume_data">
+              {resume.map((val) => {
+                if (val.category === "education") {
+                  return <ResumeItem key={val.id} {...val} />;
+                }
+              })}
+            </div>
+          </div>
+        </section>
+      </section>
+    </main>
+  );
 };
 export default About;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,38 @@
+import { Link } from "react-router-dom";
+import Profile from "../../assets/user2.jpg";
+import { FaArrowRight } from "react-icons/fa";
+import "./home.css";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home section grid">
+      <img src={Profile} alt={Profile} className="home_img" />
+
+      <div className="home_content">
+        <div className="home_data">
+          <h1 className="home_title">
+            I'm Jonah Wakahiu.
+            <span>Web designer</span>
+          </h1>
+
+          <p className="home_desc">
+            I'm a Kenyan based web designer & front-end developer focused on
+            crafting clean & user-friendly experiences, I am passionate about
+            building excellent software that improves the lives of those around
+            me.
+          </p>
+
+          <Link to="/about" className="button">
+            More About Me{" "}
+            <span className="button_icon">
+              <FaArrowRight />
+            </span>
+          </Link>
+        </div>
+      </div>
+
+      <div className="color_block"></div>
+    </section>
+  );
 };
 export default Home;
