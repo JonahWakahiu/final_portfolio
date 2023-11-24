6d688d6084c55693569c21b0c1d21e334ea99512
Jonah <jonah.wakahiumaina@gmail.com>
2023-11-24 09:09:47 +0300

    created the navbar and its functionality

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,4 +1,46 @@
+import { NavLink } from "react-router-dom";
+import { links } from "../../data";
+import { useState } from "react";
+import "./navbar.css";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleShowMenu = () => {
+    setShowMenu(!showMenu);
+  };
+  return (
+    <nav className="nav">
+      <div className={`${showMenu ? "nav_menu show-menu" : "nav_menu"}`}>
+        <ul className="nav_list">
+          {links.map(({ name, icon, path }, index) => {
+            return (
+              <li className="nav_item" key={index}>
+                <NavLink
+                  to={path}
+                  onClick={handleShowMenu}
+                  className={({ isActive }) =>
+                    isActive ? "nav_link active-nav" : "nav_link"
+                  }
+                >
+                  <div className="nav_icon">{icon}</div>
+                  <h3 className="nav_name">{name}</h3>
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div
+        onClick={handleShowMenu}
+        className={`${showMenu ? "nav_toggle animate-toggle" : "nav_toggle"}`}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
