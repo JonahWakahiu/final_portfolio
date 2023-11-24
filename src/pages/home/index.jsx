a7128c76007a8c89c18be95526da0cf544f8c98d
Jonah <jonah.wakahiumaina@gmail.com>
2023-11-24 16:59:18 +0300

    made changes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
       <div className="home_content">
         <div className="home_data">
           <h1 className="home_title">
-            <span>I'm Jonah Wakahiu.</span>
+            <span>I'm Jonah Wakahiu. </span>
             Web designer
           </h1>
 
