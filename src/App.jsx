a63c6a8cb92ff159ee5ff45b496979dbec2a2a16
Jonah <jonah.wakahiumaina@gmail.com>
2023-11-24 08:10:40 +0300

    organized the working directory to start the project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Projects from "./pages/projects";
+import Contact from "./pages/contact";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
